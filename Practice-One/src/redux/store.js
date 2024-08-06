fe870acd21a2bf513363ba75679fc5158df54753
Akhil Shah <akhil.s@lanatussystems.com>
2024-08-06 21:54:52 +0530

    Saga setup
    Saga setup

--- Practice-One/src/redux/store.js
+++ Practice-One/src/redux/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cartReducer, productReducer } from "../redux/reducers";
+import createSagaMiddleware from "redux-saga";
+import productSaga from "./saga/productSaga";
+const sagaMiddleware = createSagaMiddleware();
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     product: productReducer,
   },
+  middleware: () => [sagaMiddleware],
 });
+sagaMiddleware.run(productSaga);
