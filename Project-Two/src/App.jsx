91681ffb6f4d5e261ee74d520e61c953d853b195
Akhil Shah <akhil.s@lanatussystems.com>
2025-09-01 12:02:22 +0530

    test-automation-v3

--- Project-Two/src/App.jsx
+++ Project-Two/src/App.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const App = () => {
   // Bad code for PR test
   let unusedVar = "I am not used";
+
   consol.log("This is a typo!"); // should be console.log
+
+  if (true) {
+    // Infinite loop
+    while (true) {
+      console.log("oops");
+    }
+  }
+
+  const obj = {};
+  obj = { key: "reassigning const" }; // invalid reassign
 
   return <div>App</div>;
 };
