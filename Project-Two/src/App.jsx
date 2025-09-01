9a7f62563402eb71f2ce176b03dac1c9c7d85f21
Akhil Shah <akhil.s@lanatussystems.com>
2025-09-01 11:58:21 +0530

    pr-auto-test

--- Project-Two/src/App.jsx
+++ Project-Two/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const App = () => {
+  // Bad code for PR test
+  let unusedVar = "I am not used";
+  consol.log("This is a typo!"); // should be console.log
+
   return <div>App</div>;
 };
 
